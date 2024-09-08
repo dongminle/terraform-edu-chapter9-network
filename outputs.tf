903ac6cc297f66ce59402d513d2ef65b71ff748b
pkalsh <pkalsh@lgupluspartners.co.kr>
2024-09-08 23:12:16 +0900

    add tls private key pem output

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,16 @@ output "subnet_id_hashicat" {
 }
 
 output "security_group_id_hashicat" {
-  value = aws_security_group.hashicat.id
+  value       = aws_security_group.hashicat.id
   description = "The hashicat's security group id"
 }
 
 output "key_pair_name_hashicat" {
-  value = aws_key_pair.hashicat.key_name
+  value       = aws_key_pair.hashicat.key_name
   description = "The hashicat's key pair name"
+}
+
+output "tls_private_key_pem" {
+  value       = tls_private_key.hashicat.private_key_pem
+  description = "The hashicat's tls private key pem"
 }
