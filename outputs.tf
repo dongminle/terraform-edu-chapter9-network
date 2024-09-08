2eba4b5d5e85da31d8bc3be8f005acdd8ba5c816
pkalsh <pkalsh@lgupluspartners.co.kr>
2024-09-08 23:08:35 +0900

    add key pair name output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "security_group_id_hashicat" {
   value = aws_security_group.hashicat.id
   description = "The hashicat's security group id"
 }
+
+output "key_pair_name_hashicat" {
+  value = aws_key_pair.hashicat.key_name
+  description = "The hashicat's key pair name"
+}
